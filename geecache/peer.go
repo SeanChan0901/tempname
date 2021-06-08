afdc5c291a36f3515ef84fc52bbe8ec8c0df273a
陈曦 <chenxi.sean@bytedance.com>
2021-06-08 21:38:44 +0800

    feat: switch proto to Protocol Buffers
    Change-Id: Icdcdbdfdcbced39939866cfb92e2a18b81dbadd6

--- geecache/peer.go
+++ geecache/peer.go
@@ -1,5 +1,6 @@
 package geecache
 
+import pb "github.com/SeanChan0901/gee-cache/geecache/geecachepb"
 
 // PeerPicker is the interface that must be implemented to locate
 // the peer that owns a specific key.
@@ -9,5 +10,5 @@ type PeerPicker interface {
 
 // PeerGetter is the interface that must be implement by a peer
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
